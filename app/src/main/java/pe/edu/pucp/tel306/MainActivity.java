f4a04b7801fec03b4abbebe2710e3bab267ccff8
Leonardo <a20125980@pucp.pe>
2020-10-13 10:30:28 -0500

    leo

--- app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
@@ -1,10 +1,14 @@
 package pe.edu.pucp.tel306;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerForContextMenu(findViewById(R.id.contador));
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_context,menu);
+    }
 
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_editar:
+                Toast.makeText(this, "Boton Editar", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this,MainActivity_edit.class));
+                break;
+            case R.id.menu_reset:
+                Toast.makeText(this, "Boton Reset", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
 }
---
+++ app/src/main/java/pe/edu/pucp/tel306/MainActivity_edit.java
@@ -0,0 +1,44 @@
+package pe.edu.pucp.tel306;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity_edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_edit);
+    }
+
+    public void obtenerParametros (View view){
+        EditText editText_trabajo = findViewById(R.id.editTextNumber_trabajo);
+        String trabajo = editText_trabajo.getText().toString();
+        if( trabajo.isEmpty()){
+            editText_trabajo.setError("Debe ingresar un valor");
+        }
+        EditText editText_descanso = findViewById(R.id.editTextNumber_descanso);
+        String descanso = editText_trabajo.getText().toString();
+        if( descanso.isEmpty()){
+            editText_trabajo.setError("Debe ingresar un valor");
+        }
+        EditText editText_ciclo = findViewById(R.id.editTextNumber_ciclo);
+        String ciclo = editText_trabajo.getText().toString();
+        if( ciclo.isEmpty()){
+            editText_trabajo.setError("Debe ingresar un valor");
+        }
+
+        if(editText_trabajo.getError()== null && editText_descanso.getError() == null && editText_ciclo.getError() == null){
+            Intent intent = new Intent(MainActivity_edit.this,MainActivity.class);
+            intent.putExtra("trabajo",trabajo);
+            intent.putExtra("descanso", descanso);
+            intent.putExtra("ciclo",ciclo);
+            startActivity(intent);
+        }
+
+    }
+}
