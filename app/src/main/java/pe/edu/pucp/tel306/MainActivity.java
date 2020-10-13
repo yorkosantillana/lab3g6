3c111f9b490ce902cd9137181eb7419b8d0cafc6
Yorkman Santillana <yorkman.santillana@gmail.com>
2020-10-13 09:22:06 -0500

    Lógica de contador principal.

--- app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
@@ -1,8 +1,14 @@
 package pe.edu.pucp.tel306;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import pe.edu.pucp.tel306.ViewModels.ContadorPrincipal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Para el contador principal
+        ViewModelProvider viewModelProvider = new ViewModelProvider(this);
+        ContadorPrincipal contadorViewModel = viewModelProvider.get(ContadorPrincipal.class);
+
+        contadorViewModel.getContadorSeg().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                TextView textView = findViewById(R.id.contador3);
+                textView.setText(String.valueOf(integer));
+            }
+        });
+
+        contadorViewModel.getContadorMin().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                TextView textView = findViewById(R.id.contador2);
+                textView.setText(String.valueOf(integer));
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public void iniciarContador(View view){
+
+        ViewModelProvider viewModelProvider = new ViewModelProvider(this);
+        ContadorPrincipal contadorViewModel = viewModelProvider.get(ContadorPrincipal.class);
+        contadorViewModel.iniciarContador();
+
+
+
     }
 
 
--- app/src/main/java/pe/edu/pucp/tel306/ViewModels/ContadorPrincipal.java
+++ app/src/main/java/pe/edu/pucp/tel306/ViewModels/ContadorPrincipal.java
@@ -1,4 +1,86 @@
 package pe.edu.pucp.tel306.ViewModels;
 
-public class ContadorPrincipal {
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ContadorPrincipal extends ViewModel {
+
+    private MutableLiveData<Integer> contadorMin = new MutableLiveData<>();
+    private MutableLiveData<Integer> contadorSeg = new MutableLiveData<>();
+
+    private Thread thread = null;
+
+    public void iniciarContador(){
+
+        setThread(new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for(int contadorLocalSeg=59;contadorLocalSeg==0;contadorLocalSeg--){
+
+                    Log.d("contadorApp",String.valueOf(contadorLocalSeg));
+                    getContadorSeg().postValue(contadorLocalSeg);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+
+                }
+
+
+                for(int contadorLocalMin=25;contadorLocalMin<=10;contadorLocalMin++){
+
+                    Log.d("contadorApp",String.valueOf(contadorLocalMin));
+                    getContadorMin().postValue(contadorLocalMin);
+                    try {
+                        Thread.sleep(60000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+
+                }
+
+
+
+
+            }
+        }));
+
+        getThread().start();
+
+
+
+    }
+
+
+    public MutableLiveData<Integer> getContadorMin() {
+        return contadorMin;
+    }
+
+    public void setContadorMin(MutableLiveData<Integer> contadorMin) {
+        this.contadorMin = contadorMin;
+    }
+
+    public MutableLiveData<Integer> getContadorSeg() {
+        return contadorSeg;
+    }
+
+    public void setContadorSeg(MutableLiveData<Integer> contadorSeg) {
+        this.contadorSeg = contadorSeg;
+    }
+
+    public Thread getThread() {
+        return thread;
+    }
+
+    public void setThread(Thread thread) {
+        this.thread = thread;
+    }
+
+
 }
